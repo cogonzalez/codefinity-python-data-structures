dc43e839d3f5e25cbd2eda7ccecda994513a3b49
cogonzalez <>
2026-07-08 17:13:54 +0000

    List Mutability

--- mastering_python_lists/list_mutability/main.py
+++ mastering_python_lists/list_mutability/main.py
@@ -13,9 +13,19 @@ travel_wishlist[2][2] = travel_wishlist[2][2] * 0.8
 # Printing the updated travel_wishlist to verify the change
 print('Updated list:', travel_wishlist)
 
+# Alternatively use a for loop to iterate through the list
 # Initial travel wishlist with nested lists
+# Restore List to Original
 travel_wishlist = [
     ["Paris", "France", 2000],
     ["Tokyo", "Japan", 3000],
     ["New York", "USA", 2500]
 ]
+print('Restored list:', travel_wishlist)
+for i in range(0,len(travel_wishlist)):
+    # [][] notation refers to outte list and inner list for nested lists
+    # [i] will iterate through the list [-1] reffers to the last item in the inner list
+    # why [-1] rather than [2], you always want the program to figure it out
+    # you don't want to be counting yourself
+    travel_wishlist[i][-1] = travel_wishlist[i][-1] * 0.8
+print('Discounted list (using iteration):', travel_wishlist)

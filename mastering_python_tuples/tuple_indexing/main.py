31ddbcc63f3b70f84e20749a0b4ea233cfda3bdf
cogonzalez <>
2026-07-10 00:08:54 +0000

    Tuple indexing

---
+++ mastering_python_tuples/tuple_indexing/main.py
@@ -0,0 +1,22 @@
+space_movies = ('2001: A Space Odyssey', 'Interstellar', 'Star Wars: Episode IV - A New Hope', 'Gravity', 'The Martian')
+print("space_movies:",space_movies)
+# Write your code here
+first_movie = space_movies[0]
+last_movie = space_movies[-1]
+favorite_movies = [space_movies[-1],space_movies[-3]]
+# Testing 
+print("Favorite movies about space:", favorite_movies)
+
+
+movies = ("Inception", "Interstellar", "The Dark Knight", "Tenet", "Dunkirk", "Memento", "Following")
+print("movies:",movies)
+# Accessing the second movie
+print("Second movie in the tuple:", movies[1])
+# Accessing the sixth movie
+print("Sixth movie in the tuple:", movies[5])
+# Accessing the last movie
+print("Last movie in the tuple:", movies[-1])
+# Accessing the first movie
+print("first movie in the tuple:", movies[0])
+# Accessing the 4th from the last movie
+print("4th from last movie in the tuple:", movies[-4])

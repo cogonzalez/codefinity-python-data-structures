5b8c771451fdd3d0fa04428567ea5c586fea6b45
cogonzalez <>
2026-07-10 16:51:30 +0000

    Tuple Count

---
+++ mastering_python_tuples/tuple_count/main.py
@@ -0,0 +1,24 @@
+directors = (
+    'Steven Spielberg',
+    'Christopher Nolan',
+    'Quentin Tarantino',
+    'Martin Scorsese',
+    'Steven Spielberg',
+    'Ridley Scott',
+    'James Cameron',
+    'Steven Spielberg'
+)
+
+# Write your code here
+director = 'Steven Spielberg'
+director_count = directors.count(director)
+
+# Testing
+print(f"The number of occurrences of the director {director} is : {director_count}")
+
+
+
+
+
+
+

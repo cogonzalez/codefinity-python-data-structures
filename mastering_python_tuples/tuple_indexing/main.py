de451d58a3d539cc4a768db00de216b170b2f558
cogonzalez <>
2026-07-10 19:33:35 +0000

    Tuple Indexing

--- mastering_python_tuples/tuple_indexing/main.py
+++ mastering_python_tuples/tuple_indexing/main.py
@@ -8,15 +8,12 @@ drama_movies = (
     'The Pursuit of Happyness',
     'Schindler\'s List'
 )
-
-
 # Write your code here
 fight_club_index = drama_movies.index('Fight Club')
-
 # Testing
 print("The 'Fight Club' movie is at index:", fight_club_index)
 
-
+# ****************************
 dreamworks = (
     'Shrek the Halls',
     'Monsters vs. Aliens',
@@ -38,11 +35,30 @@ third = dreamworks.index('Madagascar', second+1)
 #fourth = dreamworks.index('Madagascar', third+1)
 print(first, second, third)
 
+print('Alternative Search Method')
 # Alternatively  ****************************
 # You don't know how many times this itme is in the list
 # You don't know when to stop searching
 # If you search beyond the occurrance you get an error
+
+s_item = 'Panda'
+if dreamworks.count(s_item) > 0:
+    stop = dreamworks.count(s_item)
+    print(f"{s_item} found {stop} occurrence(s).")
+else:
+    print(f"{s_item} not found.")
+
 found_index = 0 # this forces to start looking at index = 0
-for i in range(0,3):
-    found_index = dreamworks.index('Madagascar',found_index+i)
-    print(found_index)
+for i in range(0,stop):
+    try:
+        dreamworks.index(s_item,found_index+i)
+        found_index = dreamworks.index(s_item,found_index+i)
+        print(f"found at index: {found_index}")
+    except:
+        print('Item Not Found')
+
+
+
+
+
+

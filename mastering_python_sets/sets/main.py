7d49c57ec7603dfa0528ebea7d613b862b5c60e9
cogonzalez <>
2026-08-13 21:55:48 +0000

    Sets

--- mastering_python_sets/sets/main.py
+++ mastering_python_sets/sets/main.py
@@ -1,5 +1,6 @@
 # Write your code here
-nolan_movies = ___
+nolan_movies = {"Inception", "The Dark Knight", "Interstellar", "Tenet"}
 
 # Testing
+print(type(nolan_movies))
 print("Nolan's movies:", nolan_movies)
